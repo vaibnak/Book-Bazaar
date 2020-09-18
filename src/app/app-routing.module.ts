286473270b3e02517fa02d1d7bd68ce207f441ab
vaibnak <vaibnakgupta100@gmail.com>
2020-09-18 10:00:06 +0530

    added navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent} from './register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+	{path:'login',component: LoginComponent},
+	{path:'register',component:RegisterComponent},
+	{path:'home',component:HomeComponent},
+	{path:"",redirectTo: "/login",pathMatch:"full"},
+	{path:"**",component:LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
