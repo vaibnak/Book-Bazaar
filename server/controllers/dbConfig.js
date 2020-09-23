b4e3eb1202bd07127cca19ee3e1a5e3fc8ee9102
vaibnak <vaibnakgupta100@gmail.com>
2020-09-24 00:07:37 +0530

    small bug fixed

--- server/controllers/dbConfig.js
+++ server/controllers/dbConfig.js
@@ -1,4 +1,7 @@
 const Connection = {
-	dbUrl = "mongodb://vaibnak7:Justformlab7@ds061651.mlab.com:61651/bookbazaar",
-	dbName = "bookbazaar"
+	dbUrl: "mongodb://vaibnak7:Justformlab7@ds061651.mlab.com:61651/bookbazaar",
+	dbName: "bookbazaar"
 }
+
+module.exports = {Connection}
+
